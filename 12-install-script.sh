95085ef1c0b0be97a82ef7b047d6dc668e8e23ce
Manohar Pirla <manoharpirla123@gmail.com>
2025-01-03 17:27:26 +0530

    variables are added

--- 12-install-script.sh
+++ 12-install-script.sh
@@ -7,11 +7,13 @@ then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
 fi
-dnf list installed mysql
+ FILE=$(dnf list mysql-8.0.36-1.el9_3.x86_64)
+
+ echo "mysql service version: $FILE"
 
 if [ $? -ne 0 ]
 then # installed
-    dnf remove mysql -y
+    dnf remove mysql-8.0.36-1.el9_3.x86_64 -y
     if [ $? -ne 0 ]
     then
         echo "Removing MySQL ... FAILURE"
@@ -22,11 +24,13 @@ then # installed
 else
     echo "MySQL is already ... Removed"
 fi
-dnf list installed git
+FILE2=$(dnf list git-2.43.5-1.el9_4.x86_64)
+
+echo "Git version : $FILE2"
 
 if [ $? -ne 0 ]
 then
-    dnf remove git -y
+    dnf remove git-2.43.5-1.el9_4.x86_64 -y
     if [ $? -ne 0 ]
     then
         echo "Removing Git ... FAILURE"
