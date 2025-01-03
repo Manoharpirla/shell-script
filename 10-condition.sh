bb40b67463f3f5f583afa82b5a0a5f1e8b3ff888
Manohar Pirla <manoharpirla123@gmail.com>
2025-01-03 17:02:45 +0530

    variables are added

--- 10-condition.sh
+++ 10-condition.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Get the current day of the week (e.g., Sunday, Monday, etc.)
-day_of_week=$(date)
+day_of_week=$(date +%A)
 
 # Check if today is Sunday
-if [ "$day_of_week" -ne "Sun" ]; then
-    echo "Today is not holiday."
-    exit 1
+if [ "$day_of_week" = "Sunday" ]; then
+    echo "Today is a holiday."
 else
     echo "Today is a school day."
 fi
