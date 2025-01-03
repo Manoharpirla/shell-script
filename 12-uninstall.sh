a149aeb780a16c7b98a7d62deadb23573e0eb23c
Manohar Pirla <manoharpirla123@gmail.com>
2025-01-03 18:14:13 +0530

    variables are added

--- 12-uninstall.sh
+++ 12-uninstall.sh
@@ -7,21 +7,21 @@ then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
 fi
-dnf list installed mysql
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then # installed
-    dnf remove mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Removing Mysql ... FAILURE"
-        exit 1
-    else
-        echo "Removing MySQL ... SUCCESS"
-    fi
-else
-    echo "MySQL is already ... Removed"
-fi
+# if [ $? -ne 0 ]
+# then # installed
+#     dnf remove mysql -y
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Removing Mysql ... FAILURE"
+#         exit 1
+#     else
+#         echo "Removing MySQL ... SUCCESS"
+#     fi
+# else
+#     echo "MySQL is already ... Removed"
+# fi
 dnf list installed git
 
 if [ $? -ne 0 ]
