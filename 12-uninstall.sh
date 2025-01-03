7f73a7c85d02caec59a0422d7f77db110ad63cbc
Manohar Pirla <manoharpirla123@gmail.com>
2025-01-03 18:05:07 +0530

    variables are added

---
+++ 12-uninstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # installed
+    dnf remove mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Removing Mysql ... FAILURE"
+        exit 1
+    else
+        echo "Removing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... Removed"
+fi
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf remove git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Removin Git ... FAILURE"
+        exit 1
+    else
+        echo "Removin Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
